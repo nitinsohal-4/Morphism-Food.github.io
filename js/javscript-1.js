4a82a9fc82216437c85817a91adee5d5f254debb
Nitin kumar <98045936+Nitinkumarsohal@users.noreply.github.com>
2023-04-28 17:50:12 +0530

    web

---
+++ js/javscript-1.js
@@ -0,0 +1,8 @@
+// loader js 
+window.addEventListener("load", function () {
+const loader = document.getElementById("loader");
+setTimeout(function () {
+    loader.style.display = "none";
+}, 100);
+});
+// loader js close
